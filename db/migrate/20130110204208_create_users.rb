243dc762dd85c56800b06e1b0d4067c7d8d08036
Michal Banasiak <m.banasiak@aol.pl>
2013-01-10 21:56:20 +0100

    fixed problem with migration

--- db/migrate/20130110204208_create_users.rb
+++ db/migrate/20130110204208_create_users.rb
@@ -1,11 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.integer id
-      t.string firstName
-      t.string secondName
-      t.string description
-      t.string eMail
+      t.string :firstName
+      t.string :secondName
+      t.string :description
+      t.string :eMail
       
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130110204208) do
+
+  create_table "people", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "name"
+    t.string   "country"
+    t.integer  "age"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "firstName"
+    t.string   "secondName"
+    t.string   "description"
+    t.string   "eMail"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
