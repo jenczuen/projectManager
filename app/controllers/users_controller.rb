e797c86612e5d25a9fd7d32a1183992d263fdbb2
Michal Banasiak <m.banasiak@aol.pl>
2013-01-11 01:24:14 +0100

    users restful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,63 @@
 class UsersController < ApplicationController
-  def getUsers
+  def all
     users = User.all
 
     respond_to do |format|
       format.json { render json: users }
+    end
+  end
+
+  def get
+    user = User.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: user }
+    end
+  end
+
+  def create
+    firstName   = params[:firstName]
+    secondName  = params[:secondName]
+    eMail       = params[:eMail]
+    description = params[:description]
+    
+    User.create(firstName:   firstName,
+                secondName:  secondName,
+                eMail:       eMail,
+                description: description)
+
+    respond_to do |format|
+      format.all { render :nothing => true, :status => 200 }
+    end
+  end
+  
+  def update
+    firstName   = params[:firstName]
+    secondName  = params[:secondName]
+    eMail       = params[:eMail]
+    description = params[:description]
+    
+    user = User.find(params[:id])
+
+    user.firstName = firstName
+    user.secondName = secondName
+    user.eMail = eMail
+    user.description = description
+
+    user.save
+
+    respond_to do |format|
+      format.all { render :nothing => true, :status => 200 }
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+
+    user.delete
+
+    respond_to do |format|
+      format.all { render :nothing => true, :status => 200 }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ ProjectManager::Application.routes.draw do
   root :to => 'mainSite#index'
 
   match "/api/getCategories" => "categories#getCategories", :via => :get
-  match "/api/getUsers"      => "users#getUsers"          , :via => :get
+
+  match "/api/users/create"    => "users#create"        , :via => :post
+  match "/api/users/update"    => "users#update"        , :via => :post
+  match "/api/users/destroy"    => "users#destroy"        , :via => :post
+  match "/api/users/get"      => "users#get"          , :via => :get
+  match "/api/users/all"     => "users#all"          , :via => :get
+
   match "/api/getPosts"      => "posts#getPosts"          , :via => :get
 
 end
