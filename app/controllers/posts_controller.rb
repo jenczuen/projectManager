1bcbef1a254238f8a320fe0f995004311d2b11b7
Michal Banasiak <m.banasiak@aol.pl>
2013-01-12 20:58:07 +0100

    restful for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,49 @@
 class PostsController < ApplicationController
-  def getPosts
+  def all
     posts = Post.all
 
     respond_to do |format|
       format.json { render json: posts }
     end
   end
+
+  def get
+    post = Post.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: post }
+    end
+  end
+
+  def create
+    Post.create(title: params[:title],
+                content: params[:content])
+
+    respond_to do |format|
+      format.all { render :nothing => true, :status => 200 }
+    end
+  end
+  
+  def update
+    post = Post.find(params[:id])
+
+    post.title = params[:title]
+    post.content = params[:content]
+
+    post.save
+
+    respond_to do |format|
+      format.all { render :nothing => true, :status => 200 }
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+
+    post.delete
+
+    respond_to do |format|
+      format.all { render :nothing => true, :status => 200 }
+    end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ ProjectManager::Application.routes.draw do
   match "/api/categories/get",      to: "categories#get",       via: :get
   match "/api/categories/all",      to: "categories#all",       via: :get
 
-  match "/api/getPosts",            to: "posts#getPosts",       via: :get
+  match "/api/posts/create",        to: "posts#create",         via: :post
+  match "/api/posts/update",        to: "posts#update",         via: :post
+  match "/api/posts/destroy",       to: "posts#destroy",        via: :post
+  match "/api/posts/get",           to: "posts#get",            via: :get
+  match "/api/posts/all",           to: "posts#all",            via: :get
 
 end
