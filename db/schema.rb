d28fc346b357d173bab2173c985cc98f5806f1a1
Michal Banasiak <m.banasiak@aol.pl>
2013-01-12 18:49:03 +0100

    added index to usrs email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112162540) do
+ActiveRecord::Schema.define(:version => 20130112174745) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20130112162540) do
     t.string   "password"
   end
 
+  add_index "users", ["eMail"], :name => "index_users_on_eMail", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
