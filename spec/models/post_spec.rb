2a9fb0a954cb51aba91473ed7702c45f2b8899d7
Michal Banasiak <m.banasiak@aol.pl>
2013-01-13 15:38:24 +0100

    tests for post model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "Post model" do
+
+  before do
+    @post=Post.new
+
+    @post.title="Post"
+    @post.content="Content of post."
+  end
+  
+  subject { @post }
+
+  describe "should have proper accessor methods" do
+    it { should respond_to(:title) }
+    it { should respond_to(:content) } 
+  end
+  
+  describe "data validation" do
+    
+    describe "when all data is present" do
+      it { should be_valid }
+    end
+    
+    describe "when title is not present" do
+      before { @post.title = " " }
+      it { should_not be_valid }
+    end
+    
+    describe "when content is not present" do
+      before { @post.content = " " }
+      it { should_not be_valid }
+    end
+    
+  end
+  
+end
